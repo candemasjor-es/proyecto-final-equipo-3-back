9926ce0928f7dc4089653d8069ece1447b77178b
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-06-19 10:28:57 +0200

    fix config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    MONGO_URI: "mongodb://",
+    MONGO_URI:
+        "mongodb+srv://dguillengar:MDBadmin2334@cluster0.hcz8gv3.mongodb.net/?retryWrites=true&w=majority&appName=ProyectoFinal",
     JWT_SECRET: "tortilladepatata",
 };
