10d7143505a6ee4b1f06dacc3ab6f3b44a92b0db
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-06-19 12:21:12 +0200

    fix commit user

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     MONGO_URI:
-        "mongodb+srv://dguillengar:MDBadmin2334@cluster0.hcz8gv3.mongodb.net/?retryWrites=true&w=majority&appName=ProyectoFinal",
+        "mongodb+srv://camilomascarelljorques:eJ9gU9RYpBlKRbp4@cluster0.hcz8gv3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     JWT_SECRET: "tortilladepatata",
 };
