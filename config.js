303a2aa8014929dc1687348d53ab4589d0255329
Camilo Mascarell jorques <56049009+candemasjor-es@users.noreply.github.com>
2025-06-20 20:06:49 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGO_URI:
-        "mongodb+srv://camilomascarelljorques:eJ9gU9RYpBlKRbp4@cluster0.hcz8gv3.mongodb.net/proyecto-fin-3?retryWrites=true&w=majority&appName=Cluster0",
-    JWT_SECRET: "tortilladepatata",
-};
