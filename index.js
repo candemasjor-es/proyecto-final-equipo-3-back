ce23be32b1bb62efdbc3078fad12079b573c0431
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-06-25 10:03:47 +0200

    fix list port

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ const usersRoutes = require("./routes/user.router");
 const projectRouter = require("./routes/project.routes");
 const subprojectRouter = require("./routes/subproject.routes");
 
-
 const main = async () => {
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-  app.use("/", usersRoutes);
-  app.use("/proyectos", projectRouter);
-  app.use("/subproyectos", subprojectRouter);
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use("/", usersRoutes);
+    app.use("/proyectos", projectRouter);
+    app.use("/subproyectos", subprojectRouter);
 
-  dbConnection();
+    dbConnection();
+
+    app.listen(port, () => {
+        console.log(`App listening on ${port}`);
+    });
 };
 
 main();
