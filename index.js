ba9b200e3bc562579f7fce59c981ea812089f3a8
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-06-25 19:11:31 +0200

    fix connection

--- index.js
+++ index.js
@@ -12,17 +12,15 @@ const subprojectRouter = require("./routes/subproject.routes");
 const hoursRouter = require("./routes/hours.routes");
 
 const main = async () => {
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-  app.use("/", userRoutes);
-  app.use("/proyectos", projectRouter);
-  app.use("/subproyectos", subprojectRouter);
-  app.use("/horas", hoursRouter);
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use("/", userRoutes);
+    app.use("/proyectos", projectRouter);
+    app.use("/subproyectos", subprojectRouter);
+    app.use("/horas", hoursRouter);
 
-  app.listen(port, () => {
-    console.log(`App listening on ${port}`);
-  });
+    dbConnection();
 
     app.listen(port, () => {
         console.log(`App listening on ${port}`);
